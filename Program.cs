b0209b9fcf1017a3344f23bff2176eb62e359036
Naima Africa Francis <naima.africa@gmail.com>
2020-09-20 19:36:28 -0400

    Updated remove, transfer, and summary features.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace JurassicPark
 {
@@ -7,9 +8,9 @@ namespace JurassicPark
     {
         public string Name { get; set; } = "Unknown Dinosaur";
         public string DietType { get; set; } = "Unknown";
-        public DateTime WhenAcquired { get; set; }
+        public DateTime WhenAcquired { get; set; } = DateTime.Now;
         public double Weight { get; set; } = 0.0;
-        public int EnclosureNumber { get; set; }
+        public int EnclosureNumber { get; set; } = 00000;
 
 
 
@@ -60,52 +61,130 @@ namespace JurassicPark
             Console.WriteLine();
 
 
+
+
             Console.Write("🦖 MENU: VIEW, ADD, REMOVE, TRANSFER, SUMMARY, OR QUIT: ");
-            var usersChoice = Console.ReadLine();
-            var ultimateUsersChoice = usersChoice.ToUpper();
+            var ultimateUsersChoice = Console.ReadLine().ToUpper();
             Console.WriteLine();
             Console.WriteLine();
+
+
+            while (ultimateUsersChoice != ("QUIT"))
+            {
+
+
+                if (ultimateUsersChoice == ("VIEW"))
+                {
+                    foreach (var dinosaur in allTheDinosaurs)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"{dinosaur.Name} is a {dinosaur.DietType} that weighs {dinosaur.Weight} pounds. {dinosaur.Name} has been with us since {dinosaur.WhenAcquired} and can be found in section #{dinosaur.EnclosureNumber}.");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                    }
+                    Console.Write("🦖 MENU: VIEW, ADD, REMOVE, TRANSFER, SUMMARY, OR QUIT: ");
+                    ultimateUsersChoice = Console.ReadLine().ToUpper();
+                }
+
+
+
+                if (ultimateUsersChoice == ("ADD"))
+                {
+                    Console.Write("Name: ");
+                    var name = Console.ReadLine();
+
+                    Console.Write("Diet: ");
+                    var dietType = Console.ReadLine();
+
+                    Console.Write("Arrival Date & Time: ");
+                    var whenAcquired = DateTime.Parse(Console.ReadLine());
+
+                    Console.Write("Weight in Pounds(#'s only): ");
+                    var weight = Double.Parse(Console.ReadLine());
+
+                    Console.Write("Section #: ");
+                    var enclosureNumber = Int32.Parse(Console.ReadLine());
+
+
+                    var dinosaur = new Dinosaur()
+                    {
+                        Name = name,
+                        DietType = dietType,
+                        WhenAcquired = whenAcquired,
+                        Weight = weight,
+                        EnclosureNumber = enclosureNumber,
+                    };
+
+
+                    allTheDinosaurs.Add(dinosaur);
+
+                    Console.WriteLine($"You have added a {dietType} named {name}. {name} is {weight} pounds, is in section #{enclosureNumber} and arrived on {whenAcquired}.");
+
+
+                    Console.Write("🦖 MENU: VIEW, ADD, REMOVE, TRANSFER, SUMMARY, OR QUIT: ");
+                    ultimateUsersChoice = Console.ReadLine().ToUpper();
+                }
+
+
+
+                if (ultimateUsersChoice == ("REMOVE"))
+                {
+                    Console.Write("Which dinosaur are you deleting?  ");
+                    var dinoBeingDeleted = Console.ReadLine();
+                    var findDinoToDelete = allTheDinosaurs.Find(dinosaur => dinoBeingDeleted == dinosaur.Name);
+
+
+                    allTheDinosaurs.Remove(findDinoToDelete);
+
+                    Console.WriteLine($"{dinoBeingDeleted} has been removed from the system.");
+
+                    Console.Write("🦖 MENU: VIEW, ADD, REMOVE, TRANSFER, SUMMARY, OR QUIT: ");
+                    ultimateUsersChoice = Console.ReadLine().ToUpper();
+                }
+
+
+
+                if (ultimateUsersChoice == ("TRANSFER"))
+                {
+                    Console.Write("Which dinosaur do you want to transfer?  ");
+                    var dinoBeingMoved = Console.ReadLine();
+                    var findDinoToMove = allTheDinosaurs.Find(dinosaur => dinoBeingMoved == dinosaur.Name);
+
+                    Console.Write($"{findDinoToMove.Name} is in {findDinoToMove.EnclosureNumber}. Which enclosure number to transfer to?  ");
+                    var enclosureNumberTransferred = Int32.Parse(Console.ReadLine());
+
+                    findDinoToMove.EnclosureNumber = enclosureNumberTransferred;
+
+                    Console.WriteLine($"{findDinoToMove.Name} is now in {findDinoToMove.EnclosureNumber}.");
+
+                    Console.Write("🦖 MENU: VIEW, ADD, REMOVE, TRANSFER, SUMMARY, OR QUIT: ");
+                    ultimateUsersChoice = Console.ReadLine().ToUpper();
+                }
+
+
+
+                if (ultimateUsersChoice == ("SUMMARY"))
+                {
+                    var numberOfCarnivores = allTheDinosaurs.Count(dinosaur => dinosaur.DietType == "carnivore");
+                    var numberOfHerbivores = allTheDinosaurs.Count(dinosaur => dinosaur.DietType == "herbivore");
+                    var numberOfOmnivores = allTheDinosaurs.Count(dinosaur => dinosaur.DietType == "omnivore");
+
+                    Console.WriteLine($"The total number of carnivores is {numberOfCarnivores}");
+                    Console.WriteLine($"The total number of herbivores are {numberOfHerbivores}");
+                    Console.WriteLine($"The total number of omnivores are {numberOfOmnivores}");
+
+                    Console.Write("🦖 MENU: VIEW, ADD, REMOVE, TRANSFER, SUMMARY, OR QUIT: ");
+                    ultimateUsersChoice = Console.ReadLine().ToUpper();
+
+                }
+
+
+
+            }
 
 
             if (ultimateUsersChoice == ("QUIT"))
             { Console.WriteLine("🦖 Have a DINO-mite day!! 🦕"); }
-
-            if (ultimateUsersChoice == ("VIEW"))
-            {
-                foreach (var dinosaur in allTheDinosaurs)
-                {
-                    Console.WriteLine($"{dinosaur.Name} is a {dinosaur.DietType} that weighs {dinosaur.Weight} pounds. {dinosaur.Name} has been with us since {dinosaur.WhenAcquired} and can be found in section #{dinosaur.EnclosureNumber}.");
-                    Console.WriteLine();
-                    Console.WriteLine();
-                }
-            }
-
-            if (ultimateUsersChoice == ("ADD"))
-            {
-                Console.Write("Name: ");
-                var name = Console.ReadLine();
-
-                Console.Write("Diet: ");
-                var dietType = Console.ReadLine();
-
-                Console.Write("Arrival Date & Time: ");
-                var whenAcquired = Console.ReadLine();
-
-                Console.Write("Weight in Pounds(#'s only): ");
-                var weight = Console.ReadLine();
-
-                Console.Write("Section #: ");
-                var enclosureNumber = Console.ReadLine();
-
-                Console.WriteLine($"You have added a {dietType} named {name}. {name} is {weight} pounds and is in section #{enclosureNumber} and arrived on {whenAcquired}.");
-
-            }
-
-
-
-
-
-
 
         }
     }
